0329ea999f88620e3e0f766a4c0d3f4cd4a5bb80
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-05-02 01:50:43 -0500

    code from canvas

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -17,7 +17,9 @@ function getPizza() {
   // get pizzaInfo
   fetch(`/api/pizzas/${pizzaId}`)
     .then(response => {
+      console.log(response);
       if (!response.ok) {
+        console.log('hi');
         throw new Error({ message: 'Something went wrong!' });
       }
 
@@ -62,12 +64,14 @@ function printComment(comment) {
       <h5 class="text-dark">${comment.writtenBy} commented on ${comment.createdAt}:</h5>
       <p>${comment.commentBody}</p>
       <div class="bg-dark ml-3 p-2 rounded" >
-        ${comment.replies && comment.replies.length
-      ? `<h5>${comment.replies.length} ${comment.replies.length === 1 ? 'Reply' : 'Replies'
-      }</h5>
+        ${
+          comment.replies && comment.replies.length
+            ? `<h5>${comment.replies.length} ${
+                comment.replies.length === 1 ? 'Reply' : 'Replies'
+              }</h5>
         ${comment.replies.map(printReply).join('')}`
-      : '<h5 class="p-1">No replies yet!</h5>'
-    }
+            : '<h5 class="p-1">No replies yet!</h5>'
+        }
       </div>
       <form class="reply-form mt-3" data-commentid='${comment._id}'>
         <div class="mb-3">
@@ -107,8 +111,9 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
+
   fetch(`/api/comments/${pizzaId}`, {
-    method: "POST",
+    method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
@@ -117,14 +122,13 @@ function handleNewCommentSubmit(event) {
   })
     .then(response => {
       if (!response.ok) {
-        throw new Error({ message: 'Something went wrong!' });
+        throw new Error('Something went wrong!');
       }
-
       response.json();
     })
     .then(commentResponse => {
       console.log(commentResponse);
-      location.reload();
+      // location.reload();
     })
     .catch(err => {
       console.log(err);
@@ -148,8 +152,9 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
+
   fetch(`/api/comments/${pizzaId}/${commentId}`, {
-    method: "PUT",
+    method: 'PUT',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json'
@@ -158,9 +163,8 @@ function handleNewReplySubmit(event) {
   })
     .then(response => {
       if (!response.ok) {
-        throw new Error({ message: 'Something went wrong!' });
+        throw new Error('Something went wrong!');
       }
-
       response.json();
     })
     .then(commentResponse => {
@@ -170,12 +174,11 @@ function handleNewReplySubmit(event) {
     .catch(err => {
       console.log(err);
     });
-};
+}
 
-$backBtn.addEventListener('click', function () {
+$backBtn.addEventListener('click', function() {
   window.history.back();
 });
-
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
