f1ea45d5303bc0e863006c9db2fcfb3bbc03237c
Sheroo Bhagia <sheroo@sb-mac.lan>
2022-05-03 21:03:41 -0500

    added validations

--- models/Comment.js
+++ models/Comment.js
@@ -2,58 +2,66 @@ const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
-  {
-    // set custom id to avoid confusion with parent comment _id
-    replyId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String,
+            required: 'You need to provide a reply!',
+            trim: true
+        },
+        writtenBy: {
+            type: String,
+            required: 'You need to provide a name!',
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    replyBody: {
-      type: String
-    },
-    writtenBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
+    {
+        toJSON: {
+            getters: true
+        }
     }
-  },
-  {
-    toJSON: {
-      getters: true
-    }
-  }
 );
 
 const CommentSchema = new Schema(
-  {
-    writtenBy: {
-      type: String
+    {
+        writtenBy: {
+            type: String,
+            required: 'You need to provide a name!',
+            trim: true
+        },
+        commentBody: {
+            type: String,
+            required: 'You need to provide a comment!',
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        // use ReplySchema to validate data for a reply
+        replies: [ReplySchema]
     },
-    commentBody: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    },
-    // use ReplySchema to validate data for a reply
-    replies: [ReplySchema]
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true
-    },
-    id: false
-  }
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
 );
 
-CommentSchema.virtual('replyCount').get(function() {
-  return this.replies.length;
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
